31a434cee77b30e32626ae7ebb71b5cf8d925fbb
Alick Galvez <AlickDaSlayer@users.noreply.github.com>
2020-09-23 10:44:50 +0100

    Update subroutine_hw_task3.py

--- Worsheets/subroutine_hw_task3.py
+++ Worsheets/subroutine_hw_task3.py
@@ -1,9 +1,14 @@
-
-def addName():
-
-
 def displayName():
-
+    pass
+def addname():
+    pass
 
 run = True
 while run:
+
+    print("1. Add Name")
+    print("2. Display Names")
+    print("3. Quit")
+
+    choice = int(input("Input a number from 1-3:  "))
+    
