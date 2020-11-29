0a9b976f61e2f83fb3ebf3c5704caf5b15ac6c2f
Alick Galvez <AlickDaSlayer@users.noreply.github.com>
2020-11-29 14:17:53 +0000

    Update main.py

--- MiniProject/main.py
+++ MiniProject/main.py
@@ -1,18 +1,19 @@
 import pygame
- 
+import sys
+
 # Define some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
- 
+
 pygame.init()
 pg = pygame
- 
+
 # Set the width and height of the screen [width, height]
 size = (1280, 720)
 screen = pg.display.set_mode(size)
- 
+
 pg.display.set_caption("My Game")
 
 class Player(pg.sprite.Sprite):
@@ -24,15 +25,31 @@ class Player(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.change = -1
-        self.speed = 0
+        self.left_pressed = False
+        self.right_pressed = False
+        self.down_pressed = False
+        self.up_pressed = False
+        self.speed = 1
 
-    def movePlayer(self,x,y):
-        self.rect.x += x
-        self.rect.y += y
+##    def movePlayer(self,x,y):
+##        self.rect.x += x
+##        self.rect.y += y
 
-    def update(self):  
-        pass
+    def update(self):
+        self.velX = 0
+        self.velY = 0
+
+        if self.left_pressed and not self.right_pressed:
+            self.velX = -self.speed
+        if self.right_pressed and not self.left_pressed:
+            self.velX = self.speed
+        if self.up_pressed and not self.down_pressed:
+            self.velY = -self.speed
+        if self.down_pressed and not self.up_pressed:
+            self.velY = self.speed
+
+        self.rect.x += self.velX
+        self.rect.y += self.velY
 
 class Wall(pg.sprite.Sprite):
     def __init__(self, x, y, w, h):
@@ -46,26 +63,26 @@ class Wall(pg.sprite.Sprite):
 
 map = [
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,1,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+    [1,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
        ]
 
-        
+
 all_sprites_group = pg.sprite.Group()
 
 player = Player(500, 400, GREEN, 20, 20)
@@ -88,38 +105,67 @@ for row in map:
 
 # Loop until the user clicks the close button.
 done = False
- 
+
 # Used to manage how fast the screen updates
 clock = pg.time.Clock()
- 
+
 # -------- Main Program Loop ----------- #
 while not done:
     # --- Main event loop
     for event in pg.event.get():
         if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
             done = True
+        if event.type == pg.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.left_pressed = True
+            if event.key == pygame.K_RIGHT:
+                player.right_pressed = True
+            if event.key == pygame.K_UP:
+                player.up_pressed = True
+            if event.key == pygame.K_DOWN:
+                player.down_pressed = True
+        if event.type == pg.KEYUP:
+            if event.key == pygame.K_LEFT:
+                player.left_pressed = False
+            if event.key == pygame.K_RIGHT:
+                player.right_pressed = False
+            if event.key == pygame.K_UP:
+                player.up_pressed = False
+            if event.key == pygame.K_DOWN:
+                player.down_pressed = False
 
-    keys = pg.key.get_pressed()
+##    keys = pg.key.get_pressed()
+##
+##    if event.type == pg.KEYDOWN:
+##        if keys[pg.K_LEFT]:
+##            player.movePlayer(-1,0)
+##        if keys[pg.K_RIGHT]:
+##            player.movePlayer(1,0)
+##        if keys[pg.K_UP]:
+##            player.movePlayer(0,-1)
+##        if keys[pg.K_DOWN]:
+##            player.movePlayer(0,1)
+##    elif event.type == pg.KEYUP:
+##        player.movePlayer(0,0)
 
-    if event.type == pg.KEYDOWN:
-        if keys[pg.K_LEFT]:
-            player.movePlayer(-1,0)
-        if keys[pg.K_RIGHT]:
-            player.movePlayer(1,0)
-        if keys[pg.K_UP]:
-            player.movePlayer(0,-1)
-        if keys[pg.K_DOWN]:
-            player.movePlayer(0,1)
-    elif event.type == pg.KEYUP:
-        player.movePlayer(0,0)
-        
     # --- Game logic should go here
- 
+
+    ## -- COLLISION DETECTION OF PLAYER AND WALL SPRITES -- ##
+    player_collision = pg.sprite.spritecollide(player, wall_group, False)
+    for i in player_collision:
+        player.rect.x = old_player_x
+        player.rect.y = old_player_y
+    #next i
+
+    old_player_x = player.rect.x
+    old_player_y = player.rect.y
+
     # --- Screen-clearing code goes here
- 
     # Here, we clear the screen to white. Don't put other drawing commands
     # above this, or they will be erased with this command.
- 
+
     # If you want a background image, replace this clear with blit'ing the
     # background image.
     screen.fill(BLACK)
@@ -127,12 +173,12 @@ while not done:
 
     all_sprites_group.update()
     # --- Drawing code should go here
- 
+
     # --- Go ahead and update the screen with what we've drawn.
     pg.display.flip()
- 
+
     # --- Limit to 60 frames per second
-    clock.tick(1000)
- 
+    clock.tick(400)
+
 # Close the window and quit.
 pg.quit()
