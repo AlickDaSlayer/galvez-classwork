35a2a5a99804fe08e7385917fe41ae3003c5697f
Alick Galvez <AlickDaSlayer@users.noreply.github.com>
2021-03-10 10:04:11 +0000

    Update main.py

--- MiniProject/main.py
+++ MiniProject/main.py
@@ -10,14 +10,13 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 CYAN = (0, 255, 255)
-LIME = (100, 255, 100)
+LIME = (173, 3, 252)
 
 pg.init()
 
 # Set the width and height of the screen [width, height]
 size = (1400, 720)
 screen = pg.display.set_mode(size)
-
 font1 = pg.font.SysFont(None, 200)
 font2 = pg.font.SysFont(None, 100)
 font3 = pg.font.SysFont(None, 20)
@@ -25,6 +24,7 @@ font3 = pg.font.SysFont(None, 20)
 pg.display.set_caption("Block Charge")
 
 all_sprites_group = pg.sprite.Group()
+
 
 class Player(pg.sprite.Sprite):
     def __init__(self, x, y, colour, w, h):
@@ -57,8 +57,10 @@ class Player(pg.sprite.Sprite):
         self.rect.x += self.velX
         self.rect.y += self.velY
 
+
 player = Player(500, 400, GREEN, 20, 20)
 all_sprites_group.add(player)
+
 
 class Enemy(pg.sprite.Sprite):
     def __init__(self, x, y, colour, w, h):
@@ -93,7 +95,9 @@ class Enemy(pg.sprite.Sprite):
             self.chance_x = random.randint(1, 2)
             self.chance_y = random.randint(1, 2)
 
+
 enemy_group = pg.sprite.Group()
+
 
 class Wall(pg.sprite.Sprite):
     def __init__(self, x, y, colour, w, h):
@@ -109,31 +113,6 @@ class Wall(pg.sprite.Sprite):
 wall_group = pg.sprite.Group()
 
 
-class Sword(pg.sprite.Sprite):
-    def __init__(self, x, y, colour, w, h):
-        super().__init__()
-        self.image = pg.Surface([w,h])
-        self.image.fill(colour)
-        # Set the position of the player
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.time = None
-        self.counter = 0
-
-    #def update(self):
-        #if self.counter is not None:
-            #if self.counter == 800:
-
-
-    #def update(self):
-        #if self.time is not None:
-            #if pg.time.get_ticks() - self.time >= 10:
-                #all_sprites_group.remove(self)
-
-
-sword_group = pg.sprite.Group()
-
 def draw_map(map):
     x = 0
     y = 0
@@ -141,11 +120,12 @@ def draw_map(map):
     for row in map:
         for col in row:
             if col == 1:
-                my_wall = Wall(x, y, RED, 20, 20)
+                my_wall = Wall(x, y, WHITE, 20, 20)
                 wall_group.add(my_wall)
             x += 20
         x = 0
         y += 20
+
 
 def levels():
     draw_map(map1)
@@ -163,14 +143,43 @@ def levels():
         enemy_group.add(my_enemy)
         all_sprites_group.add(my_enemy)
 
+
 # Used to manage how fast the screen updates
 clock = pg.time.Clock()
+
 
 def draw_text(text, font, colour, surface, x, y):
     text_obj = font.render(text, 1, colour)
     text_rect = text_obj.get_rect()
     text_rect.topleft = (x, y)
     textrect_pos = screen.blit(text_obj, text_rect)
+
+
+score = 0
+round = 1
+
+
+def redraw():
+    screen.fill(BLACK)
+    all_sprites_group.draw(screen)
+    wall_group.draw(screen)
+    enemy_group.update()
+    all_sprites_group.update()
+    # --- Drawing code should go here
+    # --- Text on screen
+    draw_text(f"Score: {score}", font3, WHITE, screen, 1300, 10)
+    draw_text(f"Round: {round}", font3, WHITE, screen, 1300, 35)
+    # --- Go ahead and update the screen with what we've drawn.
+    pg.display.flip()
+
+
+def collision_test(sprite, walls):
+    hit_list = []
+    for wall in walls:
+        if sprite.colliderect(wall):
+            hit_list.append(wall)
+    return hit_list
+
 
 def main_menu():
     while True:
@@ -210,6 +219,7 @@ def main_menu():
         pg.display.update()
         clock.tick(60)
 
+
 def credits():
     while True:
         screen.fill(BLACK)
@@ -242,25 +252,22 @@ def credits():
         pg.display.update()
         clock.tick(60)
 
+
 # -------- Main Program Loop ----------- #
 def game():
 
     levels()
 
-    score = 0
-    round = 1
-
     mx, my = pg.mouse.get_pos()
 
     done = False
-    #click = False
+    click = False
     while not done:
         # --- Main event loop
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
-                done = True
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
                     pg.quit()
@@ -283,65 +290,9 @@ def game():
                     player.up_pressed = False
                 if event.key == pg.K_s:
                     player.down_pressed = False
-            if event.type == pg.MOUSEBUTTONDOWN:
-                if event.button == 1 and player.up_pressed == True:
-                    player_sword = Sword(player.rect.x+5, player.rect.y-80, BLUE, 10, 50)
-                    sword_group.add(player_sword)
-                    sword_hit_group = pg.sprite.spritecollide(player_sword, enemy_group, True)
-                    for my_enemy in sword_hit_group:
-                        enemy_group.remove(my_enemy)
-                        all_sprites_group.remove(my_enemy)
-                        score += 100
-                if event.button == 1 and player.down_pressed == True:
-                    player_sword = Sword(player.rect.x+5, player.rect.y+60, BLUE, 10, 40)
-                    sword_group.add(player_sword)
-                    sword_hit_group = pg.sprite.spritecollide(player_sword, enemy_group, True)
-                    for my_enemy in sword_hit_group:
-                        enemy_group.remove(my_enemy)
-                        all_sprites_group.remove(my_enemy)
-                        score += 100
-                if event.button == 1 and player.left_pressed == True:
-                    player_sword = Sword(player.rect.x-60, player.rect.y+5, BLUE, 50, 10)
-                    sword_group.add(player_sword)
-                    sword_hit_group = pg.sprite.spritecollide(player_sword, enemy_group, True)
-                    for my_enemy in sword_hit_group:
-                        enemy_group.remove(my_enemy)
-                        all_sprites_group.remove(my_enemy)
-                        score += 100
-                if event.button == 1 and player.right_pressed == True:
-                    player_sword = Sword(player.rect.x+60, player.rect.y+5, BLUE, 50, 10)
-                    sword_group.add(player_sword)
-                    sword_hit_group = pg.sprite.spritecollide(player_sword, enemy_group, True)
-                    for my_enemy in sword_hit_group:
-                        enemy_group.remove(my_enemy)
-                        all_sprites_group.remove(my_enemy)
-                        score += 100
-                if event.button == 3 and player.up_pressed == True:
-                    player_shield = Sword(player.rect.x-5, player.rect.y-12, CYAN, 30, 10)
-                    sword_group.add(player_shield)
-                if event.button == 3 and player.down_pressed == True:
-                    player_shield = Sword(player.rect.x-5, player.rect.y-32, CYAN, 30, 10)
-                    sword_group.add(player_shield)
-                if event.button == 3 and player.right_pressed == True:
-                    player_shield = Sword(player.rect.x-12, player.rect.y-5, CYAN, 10, 30)
-                    sword_group.add(player_shield)
-                if event.button == 3 and player.left_pressed == True:
-                    player_shield = Sword(player.rect.x-32, player.rect.y-5, CYAN, 10, 30)
-                    sword_group.add(player_shield)
-            if event.type == pg.MOUSEBUTTONUP:
-                sword_group.empty()
 
 
-        # --- Game logic should go here
-        #if click:
-            #player_sword = Sword(100, 100, BLUE, 10, 30)
-            #all_sprites_group.add(player_sword)
-            #for x in range(1, 801):
-                #player_sword.counter += 1
-
-            #player_sword.time = pg.time.get_ticks()
-
-        ## -- COLLISION DETECTION OF PLAYER AND WALL SPRITES -- ##
+        # --- COLLISION DETECTION OF PLAYER AND WALL SPRITES
         player_collision = pg.sprite.spritecollide(player, wall_group, False)
         for i in player_collision:
             player.rect.x = old_player_x
@@ -359,29 +310,11 @@ def game():
                 player.rect.y = 650
                 levels()
 
-        # Here, we clear the screen to white. Don't put other drawing commands
-        # above this, or they will be erased with this command.
+        redraw()
+        # --- Limit to 120 frames per second
+        clock.tick(120)
 
-        # If you want a background image, replace this clear with blit'ing the
-        # background image.
-        screen.fill(BLACK)
-        all_sprites_group.draw(screen)
-        wall_group.draw(screen)
-        sword_group.draw(screen)
-        sword_group.update()
-        enemy_group.update()
-        all_sprites_group.update()
-        # --- Drawing code should go here
-        # - Text on screen
-        draw_text(f"Score: {score}", font3, WHITE, screen, 1300, 10)
-        draw_text(f"Round: {round}", font3, WHITE, screen, 1300, 35)
-
-        # --- Go ahead and update the screen with what we've drawn.
-        pg.display.flip()
-
-        # --- Limit to 60 frames per second
-        clock.tick(60)
-
-main_menu()
+if __name__ == "__main__":
+    main_menu()
 
 pg.quit()
